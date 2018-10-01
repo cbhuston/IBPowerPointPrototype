f0d244395403a5b0620e61c8bd9a7a92f6bd74e9
Russell Decker <rude5139@colorado.edu>
2018-10-01 17:32:38 -0600

    function form

--- hello.py
+++ hello.py
@@ -77,50 +77,59 @@ width = Inches(1)
 left = Inches(.5)
 top = Inches(1) 
 #slide 3
-for y in range(1, 4):
-    left = Inches(.5)
-    for x in range(1,8):
-        shape = shapes.add_shape(MSO_SHAPE.RECTANGLE, left, top, width, height)
-        fill = shape.fill
-        fill.solid()
-        fill.fore_color.rgb = RGBColor(255, 255, 255)
-        line = shape.line
-        line.color.rgb = RGBColor(0,0,0)
-        line.width = Pt(2.5)
-        txBoxHeight = Inches(.25)
-        txBox1 = slide3.shapes.add_textbox(left, top-Inches(.33), width, txBoxHeight)
-        tf = txBox1.text_frame
-        p = tf.add_paragraph()
-        p.alignment = PP_ALIGN.CENTER
-        p.text = "June 2018"
-        p.font.size = Pt(10)
+def x(height, width, left, top):
+    prs = Presentation()
+    title_only_slide_layout = prs.slide_layouts[6]
+    slide = prs.slides.add_slide(title_only_slide_layout)
+    shapes = slide.shapes
+    title_only_slide_layout = prs.slide_layouts[6]
+    slide3 = prs.slides.add_slide(title_only_slide_layout)
+    shapes = slide3.shapes
+    for y in range(1, 4):
+        left = Inches(.5)
+        for x in range(1,8):
+            shape = shapes.add_shape(MSO_SHAPE.RECTANGLE, left, top, width, height)
+            fill = shape.fill
+            fill.solid()
+            fill.fore_color.rgb = RGBColor(255, 255, 255)
+            line = shape.line
+            line.color.rgb = RGBColor(0,0,0)
+            line.width = Pt(2.5)
+            txBoxHeight = Inches(.25)
+            txBox1 = slide3.shapes.add_textbox(left, top-Inches(.33), width, txBoxHeight)
+            tf = txBox1.text_frame
+            p = tf.add_paragraph()
+            p.alignment = PP_ALIGN.CENTER
+            p.text = "June 2018"
+            p.font.size = Pt(10)
 
-        picTop = top+Inches(.4)
-        pic = slide3.shapes.add_picture('key_social_logo.png', left, picTop, height=Inches(.4))
-        pic.alignment = PP_ALIGN.CENTER
-        txBoxTop = top + Inches(1.5)
-        
-        txBox3 = slide3.shapes.add_textbox(left, picTop+Inches(.1), width, Inches(.25))
-        tf = txBox3.text_frame
-        p = tf.add_paragraph()
-        p.text = "200,000,000"
-        p.font.size = Pt(10)
-        p.alignment = PP_ALIGN.CENTER
+            picTop = top+Inches(.4)
+            pic = slide3.shapes.add_picture('key_social_logo.png', left, picTop, height=Inches(.4))
+            pic.alignment = PP_ALIGN.CENTER
+            txBoxTop = top + Inches(1.5)
+            
+            txBox3 = slide3.shapes.add_textbox(left, picTop+Inches(.1), width, Inches(.25))
+            tf = txBox3.text_frame
+            p = tf.add_paragraph()
+            p.text = "200,000,000"
+            p.font.size = Pt(10)
+            p.alignment = PP_ALIGN.CENTER
 
-        txBox3 = slide3.shapes.add_textbox(left, picTop+Inches(.23), width, Inches(.25))
-        tf = txBox3.text_frame
-        p = tf.add_paragraph()
-        p.text = "IPO"
-        p.font.size = Pt(10)
-        p.alignment = PP_ALIGN.CENTER
+            txBox3 = slide3.shapes.add_textbox(left, picTop+Inches(.23), width, Inches(.25))
+            tf = txBox3.text_frame
+            p = tf.add_paragraph()
+            p.text = "IPO"
+            p.font.size = Pt(10)
+            p.alignment = PP_ALIGN.CENTER
 
-        txBox2 = slide3.shapes.add_textbox(left, top+Inches(1), width, Inches(.25))
-        tf = txBox2.text_frame
-        p = tf.add_paragraph()
-        p.text = "Bookrunner"
-        p.font.size = Pt(10)
-        p.alignment = PP_ALIGN.CENTER
-        left = left + width + Inches(.285)
-    top = top + height + Inches(.33)
+            txBox2 = slide3.shapes.add_textbox(left, top+Inches(1), width, Inches(.25))
+            tf = txBox2.text_frame
+            p = tf.add_paragraph()
+            p.text = "Bookrunner"
+            p.font.size = Pt(10)
+            p.alignment = PP_ALIGN.CENTER
+            left = left + width + Inches(.285)
+        top = top + height + Inches(.33)
 
-prs.save('test.pptx')
+    prs.save('test.pptx')
+x(height, width, left, top)
