135c5dec7b7824a2ce380f3842d8072484102b88
cbhuston <cbhuston@gmail.com>
2018-10-01 21:33:13 -0600

    commented hello.py for more testing

--- hello.py
+++ hello.py
@@ -4,79 +4,24 @@ from pptx.enum.shapes import MSO_SHAPE
 from pptx.util import Inches, Pt
 from pptx.dml.color import RGBColor
 from pptx.enum.dml import MSO_THEME_COLOR
+from PIL import Image
+
+#Current to dos in test file 10/1: 
+#Figure out image math/formula for centering 
+#Figure out exact math/formula for placing strings on varible heights 
+
 prs = Presentation()
-title_only_slide_layout = prs.slide_layouts[6]
-slide = prs.slides.add_slide(title_only_slide_layout)
-shapes = slide.shapes
-
-
-left = Inches(0.93)  # 0.93" centers this overall set of shapes
-top = Inches(3.0)
-width = Inches(1.75)
-height = Inches(1.0)
-
-shape = shapes.add_shape(MSO_SHAPE.PENTAGON, left, top, width, height)
-shape.text = 'Step 1'
-
-left = left + width - Inches(0.4)
-width = Inches(2.0)  # chevrons need more width for visual balance
-
-for n in range(2, 6):
-    shape = shapes.add_shape(MSO_SHAPE.CHEVRON, left, top, width, height)
-    shape.text = 'Step %d' % n
-    left = left + width - Inches(0.4)
-
-left = top = width = height = Inches(1.0)
-slide.shapes.add_shape(
-    MSO_SHAPE.ROUNDED_RECTANGLE, left, top, width, height
-)
-
-title_only_slide_layout = prs.slide_layouts[6]
-slide2 = prs.slides.add_slide(title_only_slide_layout)
-shapes = slide2.shapes
-
-height = Inches(1.5) 
-width = Inches(1)
-left = Inches(.5)
-top = Inches(1) 
-
-for y in range(1, 4):
-    left = Inches(.5)
-    for x in range(1,8):
-        shape = shapes.add_shape(MSO_SHAPE.RECTANGLE, left, top, width, height)
-
-        fill = shape.fill.type
-        str(fill)
-
-        textFotDateShape = shapes.add_shape(MSO_SHAPE.RECTANGLE, left, top, width, height/4)
-        txBoxHeight = Inches(.25)
-        txBox = slide2.shapes.add_textbox(left, top, width, txBoxHeight)
-        tf = txBox.text_frame
-        p = tf.add_paragraph()
-        p.text = "June 2018"
-        p.font.size = Pt(14)
-
-        picTop = top+Inches(.5)
-        pic = slide2.shapes.add_picture('key_social_logo.png', left, picTop, height=Inches(.25))
-
-        txBoxTop = top + Inches(1.5)
-        txBox = slide2.shapes.add_textbox(left, top, width, Inches(.25))
-        tf = txBox.text_frame
-        p = tf.add_paragraph()
-        p.text = "Bookrunner"
-        p.font.size = Pt(12)
-        left = left + width + Inches(.285)
-    top = top + height + Inches(.33)
-
+#slide 3
 title_only_slide_layout = prs.slide_layouts[6]
 slide3 = prs.slides.add_slide(title_only_slide_layout)
 shapes = slide3.shapes
 
+#all values all currrently hardcoded not loaded 
 height = Inches(1.5) 
 width = Inches(1)
 left = Inches(.5)
 top = Inches(1) 
-#slide 3
+
 for y in range(1, 4):
     left = Inches(.5)
     for x in range(1,8):
@@ -94,11 +39,14 @@ for y in range(1, 4):
         p.alignment = PP_ALIGN.CENTER
         p.text = "June 2018"
         p.font.size = Pt(10)
-
-        picTop = top+Inches(.4)
-        pic = slide3.shapes.add_picture('key_social_logo.png', left, picTop, height=Inches(.4))
-        pic.alignment = PP_ALIGN.CENTER
-        txBoxTop = top + Inches(1.5)
+        picTop = top+Inches(.4)#varible place image further down without effecting top
+        
+        im = Image.open('key_social_logo.png')
+        picwidth, picheight = im.size #im.size should bring in a width and height
+        # image.left = (tombstone_width - image.width) / 2 **Implemented below**
+        picleft = ((width - (picheight/picwidth)*Inches(.4))/8) + left
+        pic = slide3.shapes.add_picture('key_social_logo.png', picleft, picTop, height=Inches(.4))
+        txBoxTop = top + Inches(1.5)# varible to move txBox without effecting top 
         
         txBox3 = slide3.shapes.add_textbox(left, picTop+Inches(.1), width, Inches(.25))
         tf = txBox3.text_frame
@@ -107,12 +55,14 @@ for y in range(1, 4):
         p.font.size = Pt(10)
         p.alignment = PP_ALIGN.CENTER
 
-        txBox3 = slide3.shapes.add_textbox(left, picTop+Inches(.23), width, Inches(.25))
+        txBox3 = slide3.shapes.add_textbox(left, picTop+Inches(.23), width, Inches(.25))#hardcoded placement with extra +Inches(.23)
         tf = txBox3.text_frame
         p = tf.add_paragraph()
         p.text = "IPO"
         p.font.size = Pt(10)
         p.alignment = PP_ALIGN.CENTER
+        #Next text boxes are simpler as I just hardcode a couple extra Inches() to make this example work
+        #No function to create text box height requirements
 
         txBox2 = slide3.shapes.add_textbox(left, top+Inches(1), width, Inches(.25))
         tf = txBox2.text_frame
