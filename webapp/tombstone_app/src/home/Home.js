4509ca4c4e1d7f059b565d72fe63cb9264d70e16
Russell Decker <rude5139@colorado.edu>
2018-10-03 23:45:07 -0600

    frontend wip

---
+++ webapp/tombstone_app/src/home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = props =>(
+    <div class="jumbotron">
+    <div class = "container">
+        <h1 class="display-4">Tombstone Creator Coming Soon!</h1>
+        <p class="lead">This will soon be a web app that generate powerpoint slides containing custom tombstones for investment banking analysts</p>
+        <p>Simply upload an xls or csv file containing all tombstone info and download slides ready to go!</p>
+        <p class="lead">
+         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+    </div>
+ 
+    </div>
+
+)
+export default Home
